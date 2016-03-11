3e6a7c83a44adc0062d3cc8a22ca075ef28be646
Tasio Victoria <tasiovictoria@gmail.com>
2016-03-11 14:49:16 -0800

    inits routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,26 @@
 import utils from './utils.js'; // our custom middleware
 
 export default (app, express) => {
-  app.get('/', (req, res) => {
+  app.get('/api/auth', (req, res) => {
     res.send("Hello World!");
   });
+
+  app.get('/api/quilt', (req, res) => {
+    res.send("Hello World!");
+  });
+  app.post('/api/quilt', (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.get('/api/quilt/:id', (req, res) => {
+    res.send("Hello World!");
+  });
+  app.post('/api/quilt/:id', (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.get('/api/friends/:id', (req, res) => {
+    res.send("Hello World!");
+  });
+
 };
