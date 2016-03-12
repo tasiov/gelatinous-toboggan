f0b2169c29b3ebed28dbfeb58e6c9520c9b67f39
Griffin Michl <griffinmichl@gmail.com>
2016-03-12 15:29:16 -0800

    add current quilt reducer

--- app/src/reducers/current_quilt_reducer.js
+++ app/src/reducers/current_quilt_reducer.js
@@ -1,13 +1,17 @@
 import Immutable, { Map } from 'immutable';
 import { SET_CURRENT_QUILT, ADD_TO_CURRENT_QUILT } from '../constants/ActionTypes';
 
+const initialState = {
+  
+}
+
 export default function(state = Map(), action) {
   switch (action.type) {
     case SET_CURRENT_QUILT:
       return Immutable.fromJS(action.payload);
     case ADD_TO_CURRENT_QUILT:
       // check to make sure this is correct
-      return state.set(...action.paylod);
+      return state.set(...action.payload);
     default:
       return state;
   }
--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux-immutable';
 import user from './user_reducer';
 import friends from './friends_reducer';
 import quilts from './quilts_reducer';
+import currentQuilt from './current_quilt_reducer';
 
 const rootReducer = combineReducers({
   user,
   friends,
   quilts,
+  currentQuilt,
 });
 
 export default rootReducer;
