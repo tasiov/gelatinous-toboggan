f4b37cf5bd398d986c6ed26a048be09635f42687
Griffin Michl <griffinmichl@gmail.com>
2016-03-11 17:17:42 -0800

    implement adduser action creator

---
+++ app/src/actions/index.js
@@ -0,0 +1,12 @@
+import { ADD_USER } from '../constants/ActionTypes';
+
+let userId = 0;
+export const addUser = (username) => {
+  return {
+    type: ADD_USER,
+    payload: {
+      id: userId++,
+      username,
+    }
+  }
+}
--- app/src/reducers/user_reducer.js
+++ app/src/reducers/user_reducer.js
@@ -1,10 +1,10 @@
-import { Map } from 'immutable';
+import Immutable, { Map } from 'immutable';
 import { ADD_USER } from '../constants/ActionTypes';
 
-export default function(state = {}, action) {
+export default function(state = Map(), action) {
   switch (action.type) {
     case ADD_USER:
-      return action.payload;
+      return Immutable.fromJS(action.payload);
     default:
       return state;
   }
