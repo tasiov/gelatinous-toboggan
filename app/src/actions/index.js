00fbdd117c1dc4aa8e3c088aeb275c0cf4caa66d
Griffin Michl <griffinmichl@gmail.com>
2016-03-12 14:22:04 -0800

    define redux actions

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -1,6 +1,6 @@
-import { SET_USER } from '../constants/ActionTypes';
+import { SET_USER, START_QUILT, REQUEST_FRIENDS, RECEIVE_FRIENDS } from '../constants/ActionTypes';
 
-let userId = 0;
+let userId = 0; // for development, should be deleted once server implemented
 
 export const setUser = (username) => ({
   type: SET_USER,
@@ -9,3 +9,30 @@ export const setUser = (username) => ({
     username,
   },
 });
+
+export const startQuilt = (data) => ({
+  type: START_QUILT,
+  payload: data,
+});
+
+// get all users from server
+const requestFriends = () => ({
+  type: REQUEST_FRIENDS,
+});
+
+const receiveFriends = (friends) => ({
+  type: RECEIVE_FRIENDS,
+  payload: friends,
+});
+
+export function fetchFriends() {
+  return (dispatch) => {
+    dispatch(requestFriends());
+
+    // todo: hook up appropriately with server
+    // todo: catch errors
+    return fetch('/api/friends')
+      .then(response => response.json())
+      .then(json => dispatch(receiveFriends(json)));
+  };
+}
