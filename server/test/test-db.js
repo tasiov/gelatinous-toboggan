0a8f07d01c4fec7cad0b8da75c889ff368653afd
mjimaz <m.aljimaz@gmail.com>
2016-03-12 18:32:54 -0800

    add test for users modules

--- server/test/test-db.js
+++ server/test/test-db.js
@@ -1,19 +1,26 @@
 import Sequelize from 'sequelize';
 import { expect } from 'chai';
-import db from '../db/modules/index.js'
+import db from '../db/index.js';
+import modules from '../db/modules/index.js';
+import controllers from '../db/controllers/index.js';
 
 // testing the modules
-describe('User Model', () => {
 
-  it('should be a sqlite model', () => {
-    db.User.create({ username: 'user1' })
-    .then((user) => {
-      console.log('create:', user);
-      expect().to.be.instanceOf()
-    });
-  });
-  it('should have a schema',  () =>
-    expect(User.schema).to.exist
+describe('User Model', () => {
+  before((done) =>
+    db.sequelize.sync({ force : true }) // drops table and re-creates it
+      .then(() => done(null))
+      .catch((error) => done(error))
+    );
+
+  it('should be able to create a new user', () =>
+    modules.User.create({ username: 'maryam' })
+    .then((user) => expect(user).to.exist)
+  );
+  it('should not create duplicate users',  () =>
+  modules.User.create({ username: 'maryam' })
+  .then((user) => expect(user).to.not.exist)
+  .catch((error) => expect(error).to.exist)
   );
 });
 // testing the controllers
