3c75bbcb6a1dd693739b0aa2c150a5df22c83170
Griffin Michl <griffinmichl@gmail.com>
2016-03-23 15:32:21 -0700

    reformat response to get quests for quilts

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -3,8 +3,6 @@ import bodyParser from 'body-parser';
 
 const middleware = (app) => {
   app.use(morgan('dev'));
-  // app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  // app.use(bodyParser.json({ limit: '50mb'}));
 };
 
 export default middleware;
