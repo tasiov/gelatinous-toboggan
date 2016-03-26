11bd1fa9afb15912e53b3d22f619e9474cb23c16
Tasio Victoria <tasiovictoria@gmail.com>
2016-03-25 20:01:16 -0700

    environment fixes, edit status attr

--- server/db/controllers/index.js
+++ server/db/controllers/index.js
@@ -41,11 +41,12 @@ const verifyUser = (usernameOrEmail, password) =>
 // status 0 = pending me
 // status 1 = pending others
 // status 2 = done
-function reduceQuilts(userQuilts) {
+function mapQuilts(userQuilts) {
   return userQuilts.map(userQuilt => ({
     id: userQuilt.get('id'),
     theme: userQuilt.get('theme'),
-    status: userQuilt.get('UserQuilt').get('status') + userQuilt.get('status'),
+    // status: userQuilt.get('UserQuilt').get('status') + userQuilt.get('status'),
+    status: userQuilt.get('status'),
   })).reverse();
 }
 
@@ -53,7 +54,7 @@ function reduceQuilts(userQuilts) {
 const getAllUserQuilts = (username) =>
   getUser({ username })
     .then(user => user.getQuilts())
-    .then(reduceQuilts)
+    .then(mapQuilts)
     .catch(error => console.error('Error retrieving user\'s quilts: ', error));
 
 const getQuilt = (options) => (
@@ -63,7 +64,8 @@ const getQuilt = (options) => (
 
 const postQuilt = (options) => {
   let newQuilt;
-  return db.Quilt.create(_.pick(options, ['title', 'theme']))
+  let createObj = Object.assign({}, _.pick(options, ['title', 'theme']), {status: 0});
+  return db.Quilt.create(createObj)
     .then((quilt) => {
       newQuilt = quilt;
       return getUser({ username: options.creator.username });
